a96701da37740d9b1f06ebffa1c7794a4534f621
arnestrickmann <115920878+arnestrickmann@users.noreply.github.com>
2025-10-16 10:29:00 +0200

    fix(tooltip): raise tooltip z-index above right   sidebar (#141)

--- src/renderer/components/ui/tooltip.tsx
+++ src/renderer/components/ui/tooltip.tsx
@@ -15,7 +15,8 @@ export const TooltipContent = React.forwardRef<
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        'z-50 overflow-hidden rounded-md border px-3 py-1.5 text-xs shadow-md',
+        // High z-index to appear above sidebars, selects, modals
+        'z-[240] overflow-hidden rounded-md border px-3 py-1.5 text-xs shadow-md',
         // 2025-09-22 update: use foreground/background colors
         'bg-foreground text-background',
         'animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
